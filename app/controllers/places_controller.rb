c107f1a1c5b57efccae138e3d56e8285bb1223b5
John Moore <john@ncub8.com>
2014-04-16 20:19:32 +0000

    add user to place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
 	def index
     @places = Place.order('name').page(params[:page]).per_page(5)
 	end
@@ -8,7 +10,8 @@ class PlacesController < ApplicationController
   end 
 
   def create
-    Place.create(place_params)
+    #Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
