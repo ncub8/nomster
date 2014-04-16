74f544c269382474cadc61e70f3ce06893906170
John Moore <john@ncub8.com>
2014-04-16 01:39:27 +0000

    add form for new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,8 @@ class PlacesController < ApplicationController
 	def index
     @places = Place.order('name').page(params[:page]).per_page(5)
 	end
+  
+  def new
+    @place = Place.new
+  end  
 end
