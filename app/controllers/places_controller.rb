75700bbc49ceec5fb68fbf4c258f73f170f93869
John Moore <john@ncub8.com>
2014-04-17 14:42:49 +0000

    styling error messages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 
   def create
     #Place.create(place_params)
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def show
@@ -34,7 +38,12 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, :status => :unprocessable_entity
+    end
+    
   end
 
   def destroy
